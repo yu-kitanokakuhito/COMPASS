3ef1ba3c186c68a4e51cbe6c172c8bece55af7cb
yu-kitanokakuhito <hokkaido.yu1206@gmail.com>
2026-02-26 10:02:32 +0900

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../compass-v2.jsx'
+import App from './compass-v2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
